c93999ab1595279be4298086f3ebd6d8b0b90aff
Priyanshu Singh <priyanshu.singh2025@glbajajgroup.org>
2026-07-04 21:55:04 +0530

    Day-18 is uplaoded

---
+++ Day-18/c72.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+void sortDescending(int arr[], int n) {
+    for(int i=0; i<n-1; i++) {
+        for(int j=i+1; j<n; j++) {
+            if(arr[i] < arr[j]) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    int n;
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter elements: ");
+    for(int i=0; i<n; i++) scanf("%d", &arr[i]);
+
+    sortDescending(arr, n);
+
+    printf("Array in descending order: ");
+    for(int i=0; i<n; i++) printf("%d ", arr[i]);
+    return 0;
+}
